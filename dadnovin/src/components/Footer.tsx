b0e987f5d3c68aeb31dcc90c00ab71f94528afac
Aminmiri82 <miri.amin@ymail.com>
2025-01-29 22:21:35 +0100

    added navbar and footer, added account and chat pages

---
+++ dadnovin/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="text-center bg-black text-white py-3">
+      <p>Morteza Rajaghomi LLC</p>
+    </footer>
+  );
+};
+
+export default Footer; 
